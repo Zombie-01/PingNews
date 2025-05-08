9597ff9f8c156cb376dc6a8028ae4f3dc680a31b
adiya <mm6816557@gmail.com>
2025-05-08 11:30:08 +0800

    some serve

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar: React.FC = () => {
     <div className="fixed top-0 left-0 right-0 bg-white z-10">
       <div className="max-w-7xl mx-auto px-4 h-14 flex items-center justify-between gap-4">
         <button className="p-2 hover:bg-gray-100 rounded-full">
-          <img src={"/mask-icon.svg"} className="w-6 h-6 object-cover" />
+          <img src={"/icons/mask-icon.svg"} className="w-6 h-6 object-cover" />
         </button>
 
         <div className="flex-1 flex items-center bg-gray-100 rounded-full px-4 py-2">
